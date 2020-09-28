c7491943d2f23c1f9d7945e8841800baebaa35ba
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-28 17:29:22 +0200

    Add new font family

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -6,7 +6,7 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: "Montserrat"
-  }
+    font-family: 'Open Sans Condensed', sans-serif;
+}
 `
 export default GlobalStyles
