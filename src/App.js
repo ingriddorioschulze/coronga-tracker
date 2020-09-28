aa54e4451310dc7852c5565dc7c58edb2e0f280e
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-28 16:18:06 +0200

    Add Material UI and styled-components. Configured GlobalStyles and theme files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+
+import theme from './theme'
+import GlobalStyles from './GlobalStyles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Coronga Tracker App</header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className="App">
+        <header className="App-header">Coronga Tracker App</header>
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+const theme = {
+  colors: {},
+  font: {
+    default: 'normal normal normal Montserrat',
+    family: ' Montserrat, Calibri, Arial,',
+    size: {
+      l: '80px',
+      m: '14px',
+      s: '12px',
+    },
+    weight: {
+      bold: '700',
+    },
+  },
+}
+
+export default theme
