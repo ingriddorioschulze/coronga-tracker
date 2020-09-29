9e377c91af6dcb514a4901a546259562c87374f7
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-29 18:10:06 +0200

    Add logic to display data from the API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
+} from '@material-ui/core'
 
 import theme from './theme'
 import GlobalStyles from './GlobalStyles'
-import { FormControl, MenuItem, Select } from '@material-ui/core'
 import InfoBox from './InfoBox'
 import Map from './Map'
+import Table from './Table'
+import { sortData } from './utils'
+import LineGraph from './LineGraph'
 
 //#region styles
 const AppWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-evenly;
+  padding: 20px;
+
+  @media (max-width: 990px) {
+    flex-direction: column;
+  }
 `
 const HeaderWrapper = styled.div`
   display: flex;
@@ -25,12 +39,30 @@ const HeaderWrapper = styled.div`
 const StatsWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: 350px) {
+    flex-direction: column;
+  }
 `
+const AppLeft = styled.div`
+  flex: 0.9;
+`
+const AppRight = styled.div``
+
 //#endregion
 
 function App() {
   const [countries, setCountries] = useState([])
   const [country, setCountry] = useState('worldwide')
+  const [countryInfo, setCountryInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all ')
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data)
+      })
+  }, [])
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -41,6 +73,8 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }))
+          const sortedData = sortData(data)
+          setTableData(sortedData)
           setCountries(countries)
         })
     }
@@ -50,37 +84,69 @@ function App() {
   const onCountryChange = async (event) => {
     const countryCode = event.target.value
     setCountry(countryCode)
+
+    const url =
+      countryCode === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/all'
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode)
+        setCountryInfo(data)
+      })
   }
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <AppWrapper>
-        <HeaderWrapper>
-          <div className="title">Coronga Tracker</div>
-          <FormControl className="app__dropdown">
-            <Select
-              variant="outlined"
-              value={country}
-              onChange={onCountryChange}
-            >
-              <MenuItem value="worldwide">Worldwide</MenuItem>
-              {countries.map((country) => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </HeaderWrapper>
-        <StatsWrapper>
-          <InfoBox title="Coronavirus Cases" total={3000} cases={1234} />
-          <InfoBox title="Recovered" total={7000} cases={5678} />
-          <InfoBox title="Deaths" total={9000} cases={9078} />
-        </StatsWrapper>
-        <Map />
-
-        {/* <Table /> */}
-        {/* <Graph /> */}
-
-        {/* <Map /> */}
+        <AppLeft>
+          <HeaderWrapper>
+            <div className="title">Coronga Tracker</div>
+            <FormControl className="app__dropdown">
+              <Select
+                variant="outlined"
+                value={country}
+                onChange={onCountryChange}
+              >
+                <MenuItem value="worldwide">Worldwide</MenuItem>
+                {countries.map((country) => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </HeaderWrapper>
+          <StatsWrapper>
+            <InfoBox
+              title="Coronavirus Cases"
+              cases={countryInfo.todayCases}
+              total={countryInfo.cases}
+            />
+            <InfoBox
+              title="Recovered"
+              cases={countryInfo.todayRecovered}
+              total={countryInfo.recovered}
+            />
+            <InfoBox
+              title="Deaths"
+              cases={countryInfo.todayDeaths}
+              total={countryInfo.deaths}
+            />
+          </StatsWrapper>
+          <Map />
+        </AppLeft>
+        <AppRight>
+          <Card>
+            <CardContent>
+              <h3>Live Cases by Country</h3>
+              <Table countries={tableData} />
+              <h3>Worldwide New Cases</h3>
+              <LineGraph></LineGraph>
+            </CardContent>
+          </Card>
+        </AppRight>
       </AppWrapper>
     </ThemeProvider>
   )
