f13f7a8988e2c21579522e8c0f9ff81911a4392a
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-09-29 18:09:20 +0200

    Add table and graphs components

---
+++ src/Table.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TableWrapper = styled.div`
+  margin-top: 20px;
+  overflow: scroll;
+  height: 400px;
+  background-color: white;
+  color: #6a5d5d;
+
+  tr {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  td {
+    padding: 0.5rem;
+  }
+
+  tr:nth-of-type(odd) {
+    background-color: #f3f2f8;
+  }
+`
+
+function Table({ countries }) {
+  return (
+    <TableWrapper>
+      {countries.map(({ country, cases }) => (
+        <tr>
+          <td>{country}</td>
+          <td>
+            <strong>{cases}</strong>
+          </td>
+        </tr>
+      ))}
+    </TableWrapper>
+  )
+}
+
+export default Table
